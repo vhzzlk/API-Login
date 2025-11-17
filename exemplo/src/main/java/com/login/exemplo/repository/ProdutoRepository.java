0099ee5c950287bfc34adec972777589ca3988a0
Victor Hugo <vhzzlk@gmail.com>
2025-11-17 08:22:35 -0300

    Delete exemplo directory

--- exemplo/src/main/java/com/login/exemplo/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.login.exemplo.repository;
-
-import com.login.exemplo.entity.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository <Produto, Integer>{
-}
